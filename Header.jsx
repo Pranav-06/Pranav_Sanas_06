e077b9b4ffe918dfbfa2329d5dbd08e072164130
Pranav-06 <85402254+Pranav-06@users.noreply.github.com>
2021-06-06 21:57:09 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header(){
+  return ( 
+  <header>
+    <h1> PRANAV JEWELLERS</h1>
+  </header>
+      );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Note(){
+  return ( 
+  <div  className="note">
+    <h1>All Types of Jewellery designs are Available</h1>
+    <p>
+Address: Florida Chowk, Manjari Road, Keshavnagar, Mundhwa, Pune, Maharashtra 411036
+Hours: 
+Closed ⋅ Opens 10AM Mon
+Phone: 097646 35670</p>
+  </div>
+  );
+}
+
+export default Note;
